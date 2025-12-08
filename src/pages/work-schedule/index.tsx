afc4a95b59c13f80cf4803286e11897c566ce319
sevara-s <nematullayevasevara6@gmail.com>
2025-12-08 16:44:15 +0500

    border added and combobox fixed

--- src/pages/work-schedule/index.tsx
+++ src/pages/work-schedule/index.tsx
@@ -1,16 +1,16 @@
 import { ParamCombobox } from "@/components/as-params/combobox"
 import ParamTabs from "@/components/as-params/tabs"
 import DownloadAsExcel from "@/components/download-as-excel"
-import { employees, options } from "./constants"
+import { employees, options,years } from "./constants"
 import ScheduleTable from "./schedule-table"
 const currentMonth = new Date()
 const WorkSchedule = () => {
     return (
         <div>
-            <div className="flex items-center justify-between">
+            <div className="flex  justify-between ">
                 <ParamTabs options={options} className={"bg-transparent"} />
                 <div className="flex items-center gap-3">
-                    <ParamCombobox paramName="year" options={[]} label="Yil" className="w-full" />
+                    <ParamCombobox paramName="year" options={years} label="Yil"  addButtonProps={{size:"sm"}} />
                     <DownloadAsExcel
                         url="working-schedule"
                         name="ish-jadvali"
